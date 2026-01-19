3111ddbef1e3af9478a96bacc4c9b5eb4c5a1919
Rice <spearedrice@gmail.com>
2026-01-19 18:16:24 -0500

    basic stuff!

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const win = document.getElementById('window');
+const bar = document.getElementById('titlebar');
+
+let dragging = false;
+let startX = 0;
+let startY = 0;
+let x = 0;
+let y = 0;
+
+bar.addEventListener('mousedown', (e) => {
+  dragging = true;
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!dragging) return;
+
+  x += e.clientX - startX;
+  y += e.clientY - startY;
+
+  startX = e.clientX;
+  startY = e.clientY;
+
+  win.style.transform =
+    `translate(-50%, -50%) translate(${x}px, ${y}px)`;
+});
+
+document.addEventListener('mouseup', () => {
+  dragging = false;
+});
